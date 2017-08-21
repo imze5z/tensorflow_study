a803d9ec505729aa57c1cde0114e02ab9203e4c0
imze5z <imze5z@aliyun.com>
2017-08-21 11:29:08 -0400

    beiyesifenlei

--- an_introduction_to_machine_learning_src/1.1.py
+++ an_introduction_to_machine_learning_src/1.1.py
@@ -3,6 +3,8 @@ import sqlite3
 import random
 import time
 
+# import pudb
+
 
 class Sample(object):
     def __init__(self, shape, crust_size, crust_shade, filling_size,
@@ -18,31 +20,41 @@ class Sample(object):
 def init_db():
     conn = sqlite3.connect('1.1.db')
     c = conn.cursor()
-    tb_name = int(time.time())
-    sql = '''
-        create table tb_%d(
-        id int primary key,
-        shape text,
-        crust_size text,
-        crust_shade text,
-        filling_size text,
-        filling_shade text,
-        category text)
-        '''% tb_name
-    print(sql)
-    c.execute(sql)
-    c.close()
+    try:
+        tb_name = '1_1'
+        sql = '''
+            create table tb_%s(
+            id int primary key,
+            shape text,
+            crust_size text,
+            crust_shade text,
+            filling_size text,
+            filling_shade text,
+            category text)
+            ''' % tb_name
+        print(sql)
+        c.execute(sql)
+    except:
+        pass
+    finally:
+        c.close()
+        conn.close()
     return 'tb_' + str(tb_name)
 
 
 def insert_ex(tb_name, sample):
     conn = sqlite3.connect('1.1.db')
     c = conn.cursor()
-    args = (tb_name, sample.shape, sample.crust_size, sample.crust_shade,
-            sample.filling_size, sample.filling_shade, sample.category,)
+    args = (
+        tb_name,
+        sample.shape,
+        sample.crust_size,
+        sample.crust_shade,
+        sample.filling_size,
+        sample.filling_shade,
+        sample.category, )
     sql = ("insert into %s(shape, crust_size, crust_shade, filling_size,"
-           "filling_shade, category) "
-           "values('%s','%s','%s','%s','%s','%s')")
+           "filling_shade, category) values('%s','%s','%s','%s','%s','%s')")
     sql = sql % args
     print(sql)
     c.execute(sql)
@@ -50,9 +62,52 @@ def insert_ex(tb_name, sample):
     c.close()
     conn.close()
 
+
+def select_shape(tb_name, shape_ex):
+    # pudb.set_trace()
+    conn = sqlite3.connect('1.1.db')
+    c = conn.cursor()
+    pos_shape = ("select count(*) as pos_shape from %s where shape='%s' and "
+                 "category='pos'" % (tb_name, shape_ex))
+    c.execute(pos_shape)
+    result = c.fetchone()
+    pos_shape = float(result[0])
+
+    shape = ("select count(*) as shape from %s where shape='%s'" % (tb_name,
+                                                                    shape_ex))
+    c.execute(shape)
+    result = c.fetchone()
+    shape = float(result[0])
+
+    # 1
+    pos_shape__shape = pos_shape / shape
+    print('input: shape=%s' % shape_ex)
+    print('P(pos|shape=%s) / P(shape=%s) = %f' % (pos_shape, shape,
+                                                  pos_shape__shape))
+
+    neg_shape = ("select count(*) as neg_shape from %s where shape='%s' and"
+                 " category='neg'" % (tb_name, shape_ex))
+    c.execute(neg_shape)
+    result = c.fetchone()
+    neg_shape = float(result[0])
+
+    # 2
+    neg_shape__shape = neg_shape / shape
+    print('P(neg|shape=%s) / P(shape=%s) = %f' % (neg_shape, shape,
+                                                  neg_shape__shape))
+
+    c.close()
+    conn.close()
+    print('output:')
+    if pos_shape__shape > neg_shape__shape:
+        return 'pos'
+    else:
+        return 'neg'
+
+
 def generate_train_data():
     tbs = []
-    for j in range(10):
+    for j in range(1):
         tb_name = init_db()
         tbs.append(tb_name)
         for i in xrange(random.randint(10, 100)):
@@ -63,15 +118,14 @@ def generate_train_data():
             filling_shade = random.sample(['white', 'gray', 'dark'], 1)[0]
             category = random.sample(['pos', 'neg'], 1)[0]
 
-            sample = Sample(shape, crust_size, crust_shade,
-                                filling_size, filling_shade, category)
+            sample = Sample(shape, crust_size, crust_shade, filling_size,
+                            filling_shade, category)
             insert_ex(tb_name, sample)
         time.sleep(1)
     return tbs
 
-def train_model(tbs):
-    pass
 
 if __name__ == '__main__':
-    tbs = generate_train_data()
-    train_model(tbs)
+    tb_name = 'tb_1_1'
+    generate_train_data()
+    print(select_shape(tb_name, 'circle'))
