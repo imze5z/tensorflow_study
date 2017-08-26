2117c152ab4ea9bb3158059d819d86dec43b11ad
imze5z <imze5z@aliyun.com>
2017-08-26 04:22:37 -0400

    tensorflow，张量，计算图，计算图中定义变量，会话中读取计算图中的变量，张量的运算，张量的作用。

--- TensorFlow实战google深度学习框架_src/3.1.2.py
+++ TensorFlow实战google深度学习框架_src/3.1.2.py
@@ -16,10 +16,10 @@ import tensorflow as tf
 a = tf.constant([1.0, 2.0], name='a')
 b = tf.constant([2.0, 3.0], name='b')
 result = a + b
-print result
+print(result)
 # 通过a.graph可以查看张量所属的计算图。因为没有特意指定，所以计算图应该等于
 # 当前默认的计算图。所以下面这个操作输出值为True
-print a.graph is tf.get_default_graph()
+print(a.graph is tf.get_default_graph())
 
 g1 = tf.Graph()
 with g1.as_default():
@@ -38,17 +38,17 @@ with g2.as_default():
 with tf.Session(graph=g1) as sess:
     tf.global_variables_initializer().run()
     with tf.variable_scope('', reuse=True):
-        print sess.run(tf.get_variable('v'))
+        print(sess.run(tf.get_variable('v')))
 
 # 在计算图g2中读取变量'v'的值
 with tf.Session(graph=g2) as sess:
     tf.global_variables_initializer().run()
     with tf.variable_scope('', reuse=True):
-        print sess.run(tf.get_variable('v'))
+        print(sess.run(tf.get_variable('v')))
 
 
 g = tf.Graph()
 # 指定计算运行的设备
 with g.device('/gpu:0'):
     result = a + b
-    print result
+    print(result)
---
+++ TensorFlow实战google深度学习框架_src/3.2.1.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+'''
+张量的定义，顾名思义，张量就是保存计算的结果的引用。
+会话是用来组织数据和运算的。
+计算图是在会话中被计算的。
+变量是在计算图中存在的。
+当然tensorflow中还有一个常量的定义。
+
+还有，张量也可以脱离会话。
+'''
+
+import tensorflow as tf
+a = tf.constant([1.0, 2.0], name='a')
+b = tf.constant([2.0, 3.0], name='b')
+result = tf.add(a, b, name='add')
+print(type(a))
+print(result)
